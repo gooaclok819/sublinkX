080e42e97e140d73effb53e62e4b500e1db6e869
gooaclok819 <gooaclok819@gmail.com>
2024-04-10 20:19:22 +0800

    1.1

--- install.sh
+++ install.sh
@@ -1,93 +1,95 @@
 #!/bin/bash
 
-# 检查是否以 root 权限运行
-if [ "$EUID" -ne 0 ]
-  then echo "请以 root 权限运行此脚本."
-  exit
-fi
+# 获取最新的发行版标签
+latest_release=$(curl --silent "https://api.github.com/repos/gooaclok819/sublinkX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
-# 获取机器型号
-ARCH=$(uname -m)
+# 检测机器类型
+machine_type=$(uname -m)
 
-# 根据机器型号下载对应的发行版本
-if [ "$ARCH" = "x86_64" ]; then
-    FILE_URL=$(curl -s https://api.github.com/repos/gooaclok819/sublinkX/releases/latest | jq -r '.assets[] | select(.name == "sublink_amd64") | .browser_download_url')
-elif [ "$ARCH" = "aarch64" ]; then
-    FILE_URL=$(curl -s https://api.github.com/repos/gooaclok819/sublinkX/releases/latest | jq -r '.assets[] | select(.name == "sublink_arm64") | .browser_download_url')
+if [ "$machine_type" = "x86_64" ]; then
+    file_name="sublink_amd64"
+elif [ "$machine_type" = "aarch64" ]; then
+    file_name="sublink_arm64"
 else
-    echo "不支持的架构."
+    echo "不支持的机器类型: $machine_type"
     exit 1
 fi
 
-wget $FILE_URL -O /usr/local/bin/sublink
+# 下载文件
+curl -LO "https://github.com/gooaclok819/sublinkX/releases/download/$latest_release/$file_name"
 
-# 给二进制文件赋予执行权限
-chmod 777 /usr/local/bin/sublink
+# 设置文件为可执行
+chmod +x $file_name
 
-# 创建systemd服务
-cat << EOF > /etc/systemd/system/sublink.service
-[Unit]
+# 移动文件到/usr/local/bin
+sudo mv $file_name /usr/local/bin/sublink
+
+# 创建systemctl服务
+echo "[Unit]
 Description=Sublink Service
 
 [Service]
 ExecStart=/usr/local/bin/sublink
 
 [Install]
-WantedBy=multi-user.target
-EOF
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/sublink.service
 
-# 刷新systemd，使之能够识别新的服务
-systemctl daemon-reload
+# 启动并启用服务
+sudo systemctl start sublink
+sudo systemctl enable sublink
+
+echo "安装完成已经启动输入sublink可以呼出菜单"
 
 # 设置一个系统变量
-echo "alias sublink='bash /usr/local/bin/menu.sh'" >> /root/.bashrc
-source /root/.bashrc
+echo "alias sublink='/usr/local/bin/sublink_menu'" >> ~/.bashrc
 
-# 创建菜单脚本
-cat << EOF > /usr/local/bin/menu.sh
-#!/bin/bash
+# 创建sublink_menu.sh脚本
+echo '#!/bin/bash
 
-function check_status {
-    if systemctl -q is-active sublink; then
-        echo "当前运行状态: 已运行"
-    else
-        echo "当前运行状态: 未运行"
-    fi
-}
+while true; do
+    # 获取服务状态
+    status=$(systemctl is-active sublink)
 
-check_status
+    echo "当前运行状态: $status"
+    echo "1. 启动服务"
+    echo "2. 停止服务"
+    echo "3. 查看服务状态"
+    echo "4. 退出"
+    echo -n "请选择一个选项: "
+    read option
 
-echo "1. 安装并启动"
-echo "2. 卸载并退出"
-echo "3. 查看服务状态"
-echo "4. 退出"
+    case $option in
+        1)
+            sudo systemctl start sublink
+            systemctl daemon-reload
+            ;;
+        2)
+		        systemctl stop sublink
+		        systemctl disable sublink
+		        rm /etc/systemd/system/sublink.service
+		        systemctl daemon-reload
+		        rm  /usr/local/bin/sublink_menu
+		        rm /usr/local/bin/sublink
+		        unalias sublink
+            ;;
+        3)
+            systemctl status sublink
+            ;;
+        4)
+            exit 0
+            ;;
+        *)
+            echo "无效的选项"
+            ;;
+    esac
+done' > sublink_menu.sh
 
-read -p "请输入你的选择: " choice
+# 设置sublink_menu.sh为可执行
+chmod +x sublink_menu.sh
 
-case $choice in
-    1)
-        systemctl start sublink
-        echo "Sublink已启动."
-        ;;
-    2)
-        systemctl stop sublink
-        systemctl disable sublink
-        rm /etc/systemd/system/sublink.service
-        systemctl daemon-reload
-        echo "Sublink已卸载."
-        ;;
-    3)
-        systemctl status sublink
-        ;;
-    4)
-        echo "正在退出..."
-        ;;
-    *)
-        echo "无效的选择."
-        ;;
-esac
+# 移动sublink_menu.sh到/usr/local/bin
 
-check_status
-EOF
+sudo mv sublink_menu.sh /usr/local/bin/sublink_menu
 
-chmod +x /usr/local/bin/menu.sh
+echo "请在当前 shell 中执行以下命令来启用别名："
+echo "source ~/.bashrc"
