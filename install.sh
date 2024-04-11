30a8628ee237ec311eba5692db59b054e5abc3e5
AmamiyaHotaru <eunie@eunie.online>
2024-04-11 11:18:53 +0800

    1.2

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ fi
 cd /usr/local/bin
 mkdir sublink
 cd sublink
-echo "创建/usr/local/bin/sublink目录"
 
 # 获取最新的发行版标签
 latest_release=$(curl --silent "https://api.github.com/repos/gooaclok819/sublinkX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
@@ -54,7 +53,34 @@ echo "安装完成已经启动输入sublink或者sudo sublink可以呼出菜单"
 
 # 创建sublink_menu.sh脚本
 echo '#!/bin/bash
-
+# 定义一个函数来更新 sublink
+update_sublink() {
+    echo "开始更新 sublink..."
+    # 停止服务
+    sudo systemctl stop sublink
+    # 获取最新的发行版标签
+    latest_release=$(curl --silent "https://api.github.com/repos/gooaclok819/sublinkX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    echo "最新版本: $latest_release"
+    # 检测机器类型
+    machine_type=$(uname -m)
+    if [ "$machine_type" = "x86_64" ]; then
+        file_name="sublink_amd64"
+    elif [ "$machine_type" = "aarch64" ]; then
+        file_name="sublink_arm64"
+    else
+        echo "不支持的机器类型: $machine_type"
+        return 1
+    fi
+    # 下载文件
+    curl -LO "https://github.com/gooaclok819/sublinkX/releases/download/$latest_release/$file_name"
+    # 设置文件为可执行
+    chmod +x $file_name
+    # 移动文件到/usr/local/bin
+    sudo mv $file_name /usr/local/bin/sublink/sublink
+    # 重新启动服务
+    sudo systemctl start sublink
+    echo "sublink 更新完成."
+}
 while true; do
     # 获取服务状态
     status=$(systemctl is-active sublink)
@@ -100,31 +126,7 @@ while true; do
             echo "需要备份的目录为db,template目录为模版文件可备份可不备份"
             ;;
         6)
-            echo "开始更新 sublink..."
-            # 停止服务
-            sudo systemctl stop sublink
-            # 获取最新的发行版标签
-            latest_release=$(curl --silent "https://api.github.com/repos/gooaclok819/sublinkX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-            echo "最新版本: $latest_release"
-            # 检测机器类型
-            machine_type=$(uname -m)
-            if [ "$machine_type" = "x86_64" ]; then
-                file_name="sublink_amd64"
-            elif [ "$machine_type" = "aarch64" ]; then
-                file_name="sublink_arm64"
-            else
-                echo "不支持的机器类型: $machine_type"
-                exit 1
-            fi
-            # 下载文件
-            curl -LO "https://github.com/gooaclok819/sublinkX/releases/download/$latest_release/$file_name"
-            # 设置文件为可执行
-            chmod +x $file_name
-            # 移动文件到/usr/local/bin
-            sudo mv $file_name /usr/local/bin/sublink/sublink
-            # 重新启动服务
-            sudo systemctl start sublink
-            echo "sublink 更新完成."
+            update_sublink
             ;;
         0)
             exit 0
