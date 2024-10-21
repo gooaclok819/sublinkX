4ea8a2f99b85b81333a83c823ccfb2a2f2748d6d
AmamiyaHotaru <eunie@eunie.online>
2024-10-22 01:11:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,21 @@ while true; do
     # 获取服务状态
     status=$(systemctl is-active sublink)
     echo "当前版本: 1.6"
+    
     # 判断服务状态并打印
     if [ "$status" = "active" ]; then
         echo "当前运行状态: 已运行"
     else
         echo "当前运行状态: 未运行"
     fi
+
     echo "1. 启动服务"
     echo "2. 停止服务"
     echo "3. 卸载安装"
     echo "4. 查看服务状态"
     echo "5. 查看运行目录"
     echo "6. 修改端口"  # 新增选项
+    echo "7. 重置账户密码"  # 新增选项
     echo "0. 退出"
     echo -n "请选择一个选项: "
     read option
@@ -50,6 +53,12 @@ while true; do
             systemctl start sublink run --port "$new_port"
             systemctl daemon-reload
             echo "服务已使用新端口 $new_port 启动"
+            ;;
+        7)  # 处理重置账户密码的选项
+            echo "正在重置账户密码为默认值..."
+            systemctl start sublink setting --username admin --password 123456
+            systemctl daemon-reload
+            echo "账户密码已重置为默认值：admin/123456"
             ;;
         0)
             exit 0
