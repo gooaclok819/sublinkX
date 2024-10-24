dadc186b3ec6d0f29e05edcf5410f6c299e5567f
AmamiyaHotaru <eunie@eunie.online>
2024-10-25 02:37:32 +0800

    1.6.1

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-#创建一个程序目录
-cd /usr/local/bin
-mkdir sublink
-cd sublink
+# 创建一个程序目录
+INSTALL_DIR="/usr/local/bin/sublink"
+
+if [ ! -d "$INSTALL_DIR" ]; then
+    mkdir -p "$INSTALL_DIR"
+fi
 
 # 获取最新的发行版标签
 latest_release=$(curl --silent "https://api.github.com/repos/gooaclok819/sublinkX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 echo "最新版本: $latest_release"
+
 # 检测机器类型
 machine_type=$(uname -m)
 
@@ -31,30 +34,36 @@ curl -LO "https://github.com/gooaclok819/sublinkX/releases/download/$latest_rele
 # 设置文件为可执行
 chmod +x $file_name
 
-# 移动文件到/usr/local/bin
-sudo mv $file_name /usr/local/bin/sublink/sublink
+# 移动文件到指定目录
+mv $file_name "$INSTALL_DIR/sublink"
 
 # 创建systemctl服务
 echo "[Unit]
 Description=Sublink Service
 
 [Service]
-ExecStart=/usr/local/bin/sublink/sublink
-WorkingDirectory=/usr/local/bin/sublink
+ExecStart=$INSTALL_DIR/sublink
+WorkingDirectory=$INSTALL_DIR
 [Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/sublink.service
+WantedBy=multi-user.target" | tee /etc/systemd/system/sublink.service
+
+# 重新加载systemd守护进程
+systemctl daemon-reload
 
 # 启动并启用服务
-sudo systemctl start sublink
-sudo systemctl enable sublink
-sudo systemctl daemon-reload
+systemctl start sublink
+systemctl enable sublink
 echo "服务已启动并已设置为开机启动"
 echo "默认账号admin密码123456 端口8000"
 echo "安装完成已经启动输入sudo sublink可以呼出菜单"
 
 # 创建sublink目录
-sudo mkdir -p /usr/bin/sublink
+SublinkMenuDir="/usr/bin/sublink"
+
+if [ ! -d "$SublinkMenuDir" ]; then
+    mkdir -p "$SublinkMenuDir"
+fi
 
 # 下载menu.sh并设置权限
-sudo curl -o /usr/bin/sublink https://raw.githubusercontent.com/gooaclok819/sublinkX/main/menu.sh
-sudo chmod 755 /usr/bin/sublink
+curl -o "$SublinkMenuDir/menu.sh" https://raw.githubusercontent.com/gooaclok819/sublinkX/main/menu.sh
+chmod 755 "$SublinkMenuDir/menu.sh"
