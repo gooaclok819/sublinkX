85038dbe9e599f6d192b36b08b53a0312f914d96
AmamiyaHotaru <eunie@eunie.online>
2024-10-25 03:23:32 +0800

    1.6.1

--- menu.sh
+++ menu.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
+function Up {
+    # 获取最新的发行版标签
+    latest_release=$(curl --silent "https://api.github.com/repos/gooaclok819/sublinkX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    echo "最新版本: $latest_release"
+    # 检测机器类型
+    machine_type=$(uname -m)
+
+    if [ "$machine_type" = "x86_64" ]; then
+        file_name="sublink_amd64"
+    elif [ "$machine_type" = "aarch64" ]; then
+        file_name="sublink_arm64"
+    else
+        echo "不支持的机器类型: $machine_type"
+        exit 1
+    fi
+
+    # 下载文件
+    curl -LO "https://github.com/gooaclok819/sublinkX/releases/download/$latest_release/$file_name"
+
+    # 设置文件为可执行
+    chmod +x $file_name
+
+    # 移动文件到指定目录
+    mv $file_name "$INSTALL_DIR/sublink"
+    echo "更新完成"
+
+}
 function Select {
     # 获取最新的发行版标签
     latest_release=$(curl --silent "https://api.github.com/repos/gooaclok819/sublinkX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
     # 获取服务状态
     status=$(systemctl is-active sublink)
+    version = "1.6.1"
     echo "最新版本:$latest_release"
-    echo "当前版本: 1.6"
+    echo "当前版本:$version"
     # 判断服务状态并打印
     if [ "$status" = "active" ]; then
         echo "当前运行状态: 已运行"
@@ -18,6 +46,7 @@ function Select {
     echo "4. 查看服务状态"
     echo "5. 查看运行目录"
     echo "6. 修改端口"
+    echo "7. 更新"
     echo "0. 退出"
     echo -n "请选择一个选项: "
     read option
@@ -44,13 +73,13 @@ function Select {
                 sudo rm /etc/systemd/system/sublink.service
             fi
             # 删除相关文件和目录
-            if [ -d /usr/local/bin/sublink ]; then
-                sudo rm -r /usr/local/bin/sublink/*
+            sudo rm -r /usr/local/bin/sublink/*
+            sudo rm -r /usr/bin/sublink
+            read -p "是否保留数据(y/n): " isDelete
+            if [ "$isDelete" = "y" ]; then
+                sudo rm -r /usr/local/bin/sublink/db
                 sudo rm -r /usr/local/bin/sublink/template
                 sudo rm -r /usr/local/bin/sublink/logs
-            fi
-            if [ -d /usr/bin/sublink ]; then
-                sudo rm -r /usr/bin/sublink
             fi
             echo "卸载完成"
             ;;
@@ -61,34 +90,52 @@ function Select {
             echo "运行目录: /usr/local/bin/sublink"
             echo "需要备份的目录为db,template目录为模版文件可备份可不备份"
             cd /usr/local/bin/sublink
-            echo "已经切换到运行目录"
             ;;
         6)
             SERVICE_FILE="/etc/systemd/system/sublink.service"
             read -p "请输入新的端口号: " Port
             echo "新的端口号: $Port"
-            PARAMETER="run -port $Port"
+            PARAMETER="-port $Port"
             # 检查服务文件是否存在
             if [ ! -f "$SERVICE_FILE" ]; then
                 echo "服务文件不存在: $SERVICE_FILE"
                 exit 1
             fi
+
             # 检查 ExecStart 是否已经包含该参数
-            if grep -q "$PARAMETER" "$SERVICE_FILE"; then
-                echo "参数已存在，无需修改。"
+            if grep -q "-port" "$SERVICE_FILE"; then
+                echo "参数已存在，正在替换..."
+                # 暂停服务
+                systemctl stop sublink
+                # 使用 sed 替换 ExecStart 行中的 -port 参数
+                sudo sed -i "s/-port [0-9]\+/-port $Port/" "$SERVICE_FILE"
             else
-                #暂停服务
+                # 如果没有 -port 参数，添加新参数
+                # 暂停服务
                 systemctl stop sublink
                 # 使用 sed 替换 ExecStart 行，添加启动参数
                 sudo sed -i "/^ExecStart=/ s|$| $PARAMETER|" "$SERVICE_FILE"
                 echo "参数已添加到 ExecStart 行: $PARAMETER"
-                
-                # 重新加载 systemd 守护进程
-                sudo systemctl daemon-reload
-                # 重启 sublink 服务
-                sudo systemctl restart sublink
+            fi
 
-                echo "服务已重启。"
+            # 重新加载 systemd 守护进程
+            sudo systemctl daemon-reload
+            # 重启 sublink 服务
+            sudo systemctl restart sublink
+
+            echo "服务已重启。"
+
+            ;;
+        7)
+            # 停止服务之前检查服务是否存在
+            if systemctl is-active --quiet sublink; then
+                systemctl stop sublink
+            fi
+            # 检查是否为最新版本
+            if [[ version = latest_release ]]; then
+                echo "当前已经是最新版本"
+            else
+                Up
             fi
             ;;
         0)
