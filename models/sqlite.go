cf9e3d390d088ae10eeb976fbeb02dcb7f310218
jxqggg <jxqg597@gmail.com>
2025-01-11 21:40:48 +0800

    feat(sub): 新增订阅节点排序功能
    - 修改了sqlite在初始的时候对SubscriptionNodes表的构建
    - SubscriptionNodes类中添加了更新节点列表建立多对多关系并更新排序。
    - 在SubscriptionNodes类中添加了两个删除方法：
      - DeleteNodesBySubscriptionID(subID int) 根据订阅ID删除关联的节点排序信息。
      - DeleteNodeSortByNodeID(nodeID int) 根据节点ID删除节点排序信息。
    - 修改了Subscription的Del方法，删除订阅时清空关联订阅的节点排序信息。
    - 修改了Node的Del方法，删除节点时删除排序信息。
    - 更新了UpdateNodes方法，确保在更新节点列表时正确处理节点排序信息的保存。
    - 新增了SubUpdateSort的路由`/api/v1/subcription/sort`和前端接口UpdateSubSort
    - 前端新增在编辑订阅节点提供可修改排序的功能以及保存按钮
    - 新增了vuedraggable组件包

--- models/sqlite.go
+++ models/sqlite.go
@@ -31,7 +31,7 @@ func InitSqlite() {
 		log.Println("数据库已经初始化，无需重复初始化")
 		return
 	}
-	err = db.AutoMigrate(&User{}, &Subscription{}, &Node{}, &SubLogs{}, &SubscriptionNodes{})
+	err = db.AutoMigrate(&User{}, &Subcription{}, &Node{}, &SubLogs{}, &SubscriptionNodes{})
 	if err != nil {
 		log.Println("数据表迁移失败")
 	}
